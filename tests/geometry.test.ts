018aa36198669eb3f73a32779770ee3b439cc293
Mael Pedretti <mael.pedretti@vnv.ch>
2025-08-04 11:45:15 +0000

    chore: add more test data

--- tests/geometry.test.ts
+++ tests/geometry.test.ts
@@ -1,6 +1,12 @@
 import { segmentsCrossingPolygon, haversineDistance } from '../src/utils/geometry';
 
-const square = [[[0,0],[4,0],[4,4],[0,4],[0,0]]];
+const square = [[
+  [0, 0],
+  [4, 0],
+  [4, 4],
+  [0, 4],
+  [0, 0],
+]];
 
 interface Segment {
   geometry: { coordinates: number[][] };
@@ -8,15 +14,75 @@ interface Segment {
 }
 
 const segments: Segment[] = [
-  { geometry: { coordinates: [[-1, 2], [5, 2]] }, id: 1 }, // crosses
-  { geometry: { coordinates: [[1, 1], [3, 3]] }, id: 2 }, // inside
-  { geometry: { coordinates: [[-1, -1], [-2, -2]] }, id: 3 }, // outside
-  { geometry: { coordinates: [[1, 1], [5, 5]] }, id: 4 }, // start inside end outside
+  {
+    geometry: {
+      coordinates: [
+        [-1, 2],
+        [2, 5],
+      ]
+    }, id: 1
+  }, // starts outside, ends outside, crosses polygon twice
+  {
+    geometry: {
+      coordinates: [
+        [-1, 4],
+        [0, 5],
+      ]
+    }, id: 2
+  }, // starts outside, ends outside, does not cross polygon
+  {
+    geometry: {
+      coordinates: [
+        [-1, 1],
+        [1, 3],
+      ]
+    }, id: 3
+  }, // starts outside, ends inside
+  {
+    geometry: {
+      coordinates: [
+        [0, 1],
+        [3, 4],
+      ]
+    }, id: 4
+  }, // starts on the edge, ends on the edge
+  {
+    geometry: {
+      coordinates: [
+        [0, 0],
+        [2, 2],
+      ]
+    }, id: 5
+  }, // starts on the edge, ends inside
+  {
+    geometry: {
+      coordinates: [
+        [1, 0],
+        [5, 4],
+      ]
+    }, id: 6
+  }, // starts on the edge, ends outside, crosses polygon
+  {
+    geometry: {
+      coordinates: [
+        [4, 2],
+        [6, 4],
+      ]
+    }, id: 7
+  }, // starts on the edge, ends outside, does not cross polygon
+  {
+    geometry: {
+      coordinates: [
+        [2, 2],
+        [3, 3],
+      ]
+    }, id: 8
+  }, // starts inside, ends inside
 ];
 
 test('segmentsCrossingPolygon filters correctly', () => {
   const res = segmentsCrossingPolygon(square, segments);
-  expect(res.map((s) => s.id)).toEqual([1, 2, 4]);
+  expect(res.map((s) => s.id)).toEqual([1, 3, 4, 5, 6]);
 });
 
 test('haversineDistance gives reasonable result', () => {
@@ -28,9 +94,9 @@ import { segmentPolygonIntersections } from '../src/utils/geometry';
 test('segmentPolygonIntersections returns boundary points', () => {
   const ints = segmentPolygonIntersections(square, segments[0].geometry.coordinates);
   expect(ints).toEqual([
-    [0, 2],
-    [4, 2],
+    [0, 3],
+    [1, 4],
   ]);
-  const inside = segmentPolygonIntersections(square, segments[1].geometry.coordinates);
+  const inside = segmentPolygonIntersections(square, segments[7].geometry.coordinates);
   expect(inside.length).toBe(0);
 });
